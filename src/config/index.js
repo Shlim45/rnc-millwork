f8edee1adb6290606542b2ec8766c5970c34320f
Jonathan Hawranko <jhawranko@gmail.com>
2023-01-18 22:57:04 -0500

    Add vercel domain

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? `http://localhost:3000` : 'https://www.rncmillwork.com';
+export const server = dev ? `http://localhost:3000` : 'https://rnc-millwork.vercel.com';
