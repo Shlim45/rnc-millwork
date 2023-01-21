013b818a9e5471a41a8b9b625552c9d6b4f04bd9
Jonathan Hawranko <jhawranko@gmail.com>
2023-01-20 22:54:29 -0500

    Update META keywords

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,16 @@ export default function Home() {
       <Head>
         <title>RnC Custom Millwork</title>
         <meta name="description" content="Custom woodworking millwork shop" />
-        <meta name="keywords" content="woodworking, custom wood, table, desk, cabinet, pittsburgh, millwork, built in, built-in, hand-made, hand made" />
+        <meta name="keywords" content="woodworking, custom wood, table, desk, cabinet, pittsburgh, millwork, built in, built-in, hand-made, hand made, pittsburgh, south hills, north hills" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Image src={sawStopPic} alt={"Lumber resting atop a SawStop."} priority />
+      <Image 
+        src={sawStopPic} 
+        alt={"Lumber resting atop a SawStop."} 
+        priority
+      />
 
       <h1>Home page</h1>
     </>
