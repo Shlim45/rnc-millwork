1a5e50bfff4662bcb2c7dcb1f1138aca98aa1c3b
Jonathan Hawranko <jhawranko@gmail.com>
2023-01-22 12:26:52 -0500

    Add responsive breakpoints to HeaderImage.module.css

--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import styles from "@/styles/HeaderImage.module.css";
 
+/* 
+    {
+        src: '/_next/static/media/lumber_on_sawstop_blurred.79ba599e.webp',
+        height: 600,
+        width: 800,
+        blurDataURL: '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flumber_on_sawstop_blurred.79ba599e.webp&w=8&q=70',
+        blurWidth: 8,
+        blurHeight: 6
+    }
+*/
+
 const HeaderImage = ( {src, alt} ) => {
-    console.log(src);
     return (
         <div>
             <Image 
