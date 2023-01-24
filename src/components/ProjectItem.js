c8b4fa790b3d57c5813ee2e9bbec9686ea0300a9
Jonathan Hawranko <jhawranko@gmail.com>
2023-01-24 17:32:32 -0500

    Fixes

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,7 @@ const ProjectItem = ({project}) => {
   return (
     <Link className={projectStyles.card} href="/project/[id]" as={`/project/${project.id}`}>
         <h3>{project.title} <span className={projectStyles.arrow}>&rarr;</span></h3>
-        <Image src={`/projects/${project.id}/${project.images[project.cover]}`} key={project.id} alt={project.alts[0]} className={projectStyles.card__image}  width={400} height={400} />
+        <Image src={`/projects/${project.id}/${project.images[project.cover]}`} key={project.id} alt={project.alts[0]} className={projectStyles.card__image}  width={400} height={400} priority />
         <p>{project.body}</p>
     </Link>
   )
