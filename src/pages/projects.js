47fadcfdf76f20cfb82b4b3e6d5e657e52389c8a
Jonathan Hawranko <jhawranko@gmail.com>
2023-01-31 10:08:29 -0500

    Fix icon uri

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,7 +10,7 @@ export default function Projects({ projects }) {
         <meta name="description" content="Custom woodworking millwork shop located near Pittsburgh, PA" />
         <meta name="keywords" content="rc millwork, rcm, millwork, mill work, woodworking, custom wood, table, desk, cabinet, pittsburgh, millwork, built in, built-in, hand-made, hand made, pittsburgh, south hills, north hills" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/rcm-icon.png" />
       </Head>
 
       <h1>Projects</h1>
