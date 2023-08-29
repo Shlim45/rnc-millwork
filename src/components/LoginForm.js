9be81d5626c164dbba4e40a68e6dd1aa3dd0a26d
Jonathan Hawranko <jhawranko@gmail.com>
2023-08-29 23:40:30 +0000

    Add div to admin login page for error feedback

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,34 +2,58 @@ import FormWrapper from "./FormWrapper";
 import { server } from "@/config";
 import { supabase } from "@/utils/supabaseClient";
 import styles from '@/styles/Home.module.css'
+import { useState } from "react";
 
 const LoginForm = () => {
-    // Handles the submit event on form submit.
+    const [loading, setLoading] = useState(true);
+    const [message, setMessage] = useState();
+    const [success, setSuccess] = useState(false);
+
     const handleSubmit = async (event) => {
-        // Stop the form from submitting and refreshing the page.
         event.preventDefault();
 
+        // if (loading) { return; }
 
-        const { data, error } = await supabase.auth.signInWithOtp({
-            email: event.target.email.value,
-            options: {
-                emailRedirectTo: `${server}/admin`,
-            },
-        });
+        try {
+            setLoading(true);
+            setSuccess(false);
+            setMessage(undefined);
 
-        if (error) {
-            alert(error);
-            console.error(error);
+            const { data, error } = await supabase.auth.signInWithOtp({
+                email: event.target.email.value,
+                options: {
+                    emailRedirectTo: `${server}/admin`,
+                },
+            });
+
+            if (error) {
+                console.error(error);
+                throw error;
+            }
+
+            setSuccess(true);
+            setMessage(`A log in link has been sent to the given email address.`);
+
+            if (data) {
+                console.log(data);
+            }
+
         }
-
-        if (data) {
-            console.log(data);
+        catch (error) {
+            setMessage(`Log in error: ${error.message}`);
+        }
+        finally {
+            setLoading(false);
         }
     }
 
     return (
-        // We pass the event to the handleSubmit() function on submit.
         <section className={styles.sandbox}>
+            <div style={{ height: "100px", fontSize: "1rem" }}>
+                {message &&
+                    <span style={success ? { color: "lightgreen" } : { color: "red" }}>{message}</span>}
+            </div>
+
             <h1 className={styles.sandbox__header}><span>Log in</span></h1>
 
             <FormWrapper handleSubmit={handleSubmit}>
