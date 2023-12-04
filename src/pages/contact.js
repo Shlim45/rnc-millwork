31843d3f811a092c7038599f211748c26349b3c6
Jonathan Hawranko <jhawranko@gmail.com>
2023-12-03 22:00:29 -0500

    Updating light-mode styles, contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,11 +21,14 @@ const contact = () => {
                 <p>Contact us today to learn more about how we can help you create the perfect piece for your home or business!</p>
                 <p>Email: <Link href="mailto:roetterbill@rccustommillworks.com">RoetterBill@RCCustomMillworks.com</Link>
                     <br />
-                    Phone: (412) 926-2009</p>
-                <address>
-                    951 Killarney Dr <br />
-                    Pittsburgh, PA 15234
-                </address>
+                    Phone: <Link href="tel:412-207-7150">(412) 207-7150</Link></p>
+
+                <Link target="_blank" href="https://www.google.com/maps/place/RC+Custom+Millworks/@40.375231,-80.0094105,17z/data=!4m14!1m7!3m6!1s0x654ad548cd47a71f:0x25f5cc7671714823!2sRC+Custom+Millworks!8m2!3d40.375231!4d-80.0068356!16s%2Fg%2F11svwv9nzk!3m5!1s0x654ad548cd47a71f:0x25f5cc7671714823!8m2!3d40.375231!4d-80.0068356!16s%2Fg%2F11svwv9nzk?entry=ttu">
+                    <address>
+                        951 Killarney Dr <br />
+                        Pittsburgh, PA 15234
+                    </address>
+                </Link>
             </SandBox>
         </>
     )
