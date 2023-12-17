16be6df7148604ccbc922b66d2c1c33e5b78697b
Jonathan Hawranko <jhawranko@gmail.com>
2023-12-17 18:32:09 +0000

    Consolidating database fetching for projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,82 @@
-import ProjectList from '@/components/projects/ProjectList';
+import React, { useState } from 'react'
 import { supabase } from '@/utils/supabaseClient';
 import { NextSeo } from 'next-seo';
+import Link from 'next/link';
 
-export default function Projects({ projects }) {
+import ImageAsideText from '@/components/ImageAsideText';
+import { joinClassNames } from '@/utils'
+import styles from '@/styles/projects/ProjectList.module.css'
+
+
+const ProjectNav = ({ categories, selected, handleSelect }) => {
+    return (
+        <nav className={styles.projectNav}>
+            <h3>Categories</h3>
+            {categories?.map((category, index) => (
+                <Link
+                    key={index}
+                    onClick={() => handleSelect(index)}
+                    className={index === selected ? (joinClassNames(styles.navLink, styles.navLink__selected)) : styles.navLink}
+                    href={`#${category.name}`}
+                >
+                    {category.name}
+                </Link>
+            ))}
+        </nav>
+    );
+}
+
+const ProjectCategory = ({ name, projects, isMobile = false }) => {
+    return (
+        <>
+            <a id={name} />
+            <div className={styles.card} key={name}>
+                <h2><span>{name}</span></h2>
+                {projects?.map((project, index) => (
+                    <ImageAsideText
+                        key={project.id}
+                        imageURL={`projects/${project.id}/${project.images[project.cover]}`}
+                        imageAlt={project.alts[project.cover]}
+                        heading={project.title}
+                        text={project.body}
+                        link={`/project/${project.id}`}
+                        imageLeft={isMobile || (index % 2 === 0)}
+                    />
+                ))}
+            </div>
+        </>
+
+
+    )
+
+}
+
+const ProjectList = ({ projects, categories, selected = 0, handleSelect, isMobile }) => {
+    return (
+        <section className={styles.listContainer}>
+            <ProjectNav categories={categories} selected={selected} handleSelect={handleSelect} />
+            <div className={styles.rightCol}>
+                {categories?.map((category, index) => {
+                    let matchingProjects = projects?.filter(pro => pro.categories?.includes(category.name));
+                    if (matchingProjects?.length > 0) {
+                        return (
+                            <ProjectCategory name={category.name} projects={matchingProjects} key={index} isMobile={isMobile} />
+                        );
+                    }
+                })}
+            </div>
+        </section>
+    )
+}
+
+
+export default function Projects({ projects, categories }) {
+    const [selected, setSelected] = useState(0);
+
+    const handleSelect = index => setSelected(index);
+
+    const isMobile = (typeof window !== "undefined") ? window.innerWidth <= 700 : false;
+
     return (
         <>
             <NextSeo
@@ -17,18 +91,28 @@ export default function Projects({ projects }) {
                 ]}
 
             />
-
-            <ProjectList projects={projects} />
+            <ProjectList projects={projects} categories={categories} handleSelect={handleSelect} selected={selected} isMobile={isMobile} />
         </>
     )
 }
 
 export const getStaticProps = async () => {
-    let { data } = await supabase.from('projects').select('*').eq('hidden', false);
+    let { data: projectData } = await supabase.from('projects').select('*').eq('hidden', false);
+    let { data: categoryData } = await supabase.from('categories').select().order('id');
+    let { data: projectCategoryData } = await supabase.from('project_categories').select();
+
+    const projects = projectData.map(project => {
+        const cats = projectCategoryData.filter(cat => cat.project_id == project.id);
+        return {
+            ...project,
+            "categories": cats.map(pCat => pCat.category_name)
+        }
+    }).sort((a, b) => a.id - b.id);;
 
     return {
         props: {
-            projects: data
+            projects,
+            categories: categoryData
         },
         // Next.js will attempt to re-generate the page:
         // - When a request comes in
